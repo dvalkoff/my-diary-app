76bf31d4ae8b3bab738351c95e2c9610d6cd5226
Dmitry Valkov <er_radd@mail.ru>
2021-08-16 21:02:55 +0300

    Initial commit

---
+++ src/main/java/ru/valkov/spring/mydiaryapp/MyDiaryAppApplication.java
@@ -0,0 +1,13 @@
+package ru.valkov.spring.mydiaryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDiaryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDiaryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/valkov/spring/mydiaryapp/MyDiaryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.valkov.spring.mydiaryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDiaryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
