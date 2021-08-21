e166c37b3a0a21390594111120d7daf87caa26c7
Dmitry Valkov <er_radd@mail.ru>
2021-08-21 16:31:21 +0300

    mail sender for deploy

--- src/main/java/ru/valkov/spring/mydiaryapp/registration/SignController.java
+++ src/main/java/ru/valkov/spring/mydiaryapp/registration/SignController.java
@@ -1,8 +1,6 @@
 package ru.valkov.spring.mydiaryapp.registration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +17,7 @@ public class SignController {
 
     @GetMapping("/login")
     public String getLoginView() {
-        return "/registration/login";
+        return "login";
     }
 
     @GetMapping("/sign-up")
